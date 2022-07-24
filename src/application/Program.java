1c92662b346c96cd310d70fc69b267d90dc175a2
SilvioRodriguesFernandes <cleniolelys@gmail.com>
2022-07-24 18:08:01 -0400

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -25,5 +25,13 @@ public class Program {
 			System.out.println(sel);
 			
 		}
+		
+		System.out.println();
+		System.out.println("==== TEST 3: seller findAll ====");
+		list = sellerDao.findAll();
+		for (Seller sel : list) {
+			System.out.println(sel);
+			
+		}
 	}
 }
